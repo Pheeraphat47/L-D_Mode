44cd9f1aaa7aa1bd032d69817b59cf3ced80e9a3
Pheeraphat <dpheeraphat2547@gmail.com>
2023-03-26 19:55:48 +0700

    finish first draft projects

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,14 +1,17 @@
-import light from '../assets/image/light.svg';
-import dark from '../assets/image/dark.svg';
+import light from '../assets/image/light.svg'
+import dark from '../assets/image/dark.svg'
+import { useContext } from 'react'
+import { ThemeContext } from '../App'
 
-function Content(){
+const Content = ()=>{
+    const {theme} = useContext(ThemeContext)
     return(
-        <main>
+        <main className={theme==="dark"?"dark":"light"}>
             <div>
-                <h1>Road to Frontend Dev</h1>
-                <p>what you have to learn ? </p>
+                <h1>Road to Frontend Developer</h1>
+                <p>What you have to learn ?</p>
             </div>
-            <img src={light} alt="logo" />
+            <img src={theme==="dark"? dark : light} alt="Logo"/>
         </main>
     )
 }
--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,25 @@
-function Title(){
-    return (
-        <header>
-            <span>Mode</span>
+import { useContext, useState } from "react"
+import Switch from "react-switch"
+import { ThemeContext } from "../App"
+const Title = ()=>{
+    const {theme,setTheme} = useContext(ThemeContext)
+
+    const toggleSwitch = (checked) =>{
+        setTheme(
+            theme === "light" ? "dark" : "light"
+        )
+        document.documentElement.classList.toggle('dark', theme === 'light')
+    }
+    return(
+        <header className={theme==="dark"?"dark":"light"}>
+            <span>Mode [{theme}]</span>
+            <Switch
+                onChange = {toggleSwitch} 
+                checked = {theme==="dark"}
+                uncheckedIcon={false}
+                checkedIcon={false}
+                onColor={'#ffa500'}
+            />
         </header>
     )
 }
